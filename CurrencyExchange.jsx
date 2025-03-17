166eb674ee8219988760f867fdf25af7174cebb0
Ölvödi Soma <94902475+olvodisoma@users.noreply.github.com>
2025-03-17 15:02:01 +0100

    currency oldal

---
+++ CurrencyExchange.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import DataTable from "react-data-table-component";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const columns = [
+  { name: "Number", selector: (row) => row.number, sortable: true },
+  { name: "Currency Name", selector: (row) => row.currencyName, sortable: true },
+  { name: "Country", selector: (row) => row.country, sortable: true },
+  { name: "Exchange Rate", selector: (row) => row.exchangeRate, sortable: true },
+];
+
+const data = [
+  { number: "1", currencyName: "USD", country: "America", exchangeRate: 374.5 },
+  { number: "2", currencyName: "GBP", country: "United Kingdom", exchangeRate: 486.71 },
+  { number: "3", currencyName: "Sravani", country: "Australia", exchangeRate: 7000 },
+  { number: "4", currencyName: "Amar", country: "India", exchangeRate: 18000 },
+  { number: "5", currencyName: "Lakshmi", country: "India", exchangeRate: 12000 },
+  { number: "6", currencyName: "James", country: "Canada", exchangeRate: 50000 },
+  { number: "7", currencyName: "Ronald", country: "US", exchangeRate: 75000 },
+  { number: "8", currencyName: "Mike", country: "Belgium", exchangeRate: 100000 },
+  { number: "9", currencyName: "Andrew", country: "Argentina", exchangeRate: 45000 },
+  { number: "10", currencyName: "Stephen", country: "Austria", exchangeRate: 30000 },
+  { number: "11", currencyName: "Sara", country: "China", exchangeRate: 750000 },
+  { number: "12", currencyName: "JonRoot", country: "Argentina", exchangeRate: 65000 },
+  { number: "1", currencyName: "USD", country: "America", exchangeRate: 374.5 },
+  { number: "2", currencyName: "GBP", country: "United Kingdom", exchangeRate: 486.71 },
+  { number: "3", currencyName: "Sravani", country: "Australia", exchangeRate: 7000 },
+  { number: "4", currencyName: "Amar", country: "India", exchangeRate: 18000 },
+  { number: "5", currencyName: "Lakshmi", country: "India", exchangeRate: 12000 },
+  { number: "6", currencyName: "James", country: "Canada", exchangeRate: 50000 },
+  { number: "7", currencyName: "Ronald", country: "US", exchangeRate: 75000 },
+  { number: "8", currencyName: "Mike", country: "Belgium", exchangeRate: 100000 },
+  { number: "9", currencyName: "Andrew", country: "Argentina", exchangeRate: 45000 },
+  { number: "10", currencyName: "Stephen", country: "Austria", exchangeRate: 30000 },
+  { number: "11", currencyName: "Sara", country: "China", exchangeRate: 750000 },
+  { number: "12", currencyName: "JonRoot", country: "Argentina", exchangeRate: 65000 },
+  { number: "1", currencyName: "USD", country: "America", exchangeRate: 374.5 },
+  { number: "2", currencyName: "GBP", country: "United Kingdom", exchangeRate: 486.71 },
+  { number: "3", currencyName: "Sravani", country: "Australia", exchangeRate: 7000 },
+  { number: "4", currencyName: "Amar", country: "India", exchangeRate: 18000 },
+  { number: "5", currencyName: "Lakshmi", country: "India", exchangeRate: 12000 },
+  { number: "6", currencyName: "James", country: "Canada", exchangeRate: 50000 },
+  { number: "7", currencyName: "Ronald", country: "US", exchangeRate: 75000 },
+  { number: "8", currencyName: "Mike", country: "Belgium", exchangeRate: 100000 },
+  { number: "9", currencyName: "Andrew", country: "Argentina", exchangeRate: 45000 },
+  { number: "10", currencyName: "Stephen", country: "Austria", exchangeRate: 30000 },
+  { number: "11", currencyName: "Sara", country: "China", exchangeRate: 750000 },
+  { number: "12", currencyName: "JonRoot", country: "Argentina", exchangeRate: 65000 },
+  { number: "1", currencyName: "USD", country: "America", exchangeRate: 374.5 },
+  { number: "2", currencyName: "GBP", country: "United Kingdom", exchangeRate: 486.71 },
+  { number: "3", currencyName: "Sravani", country: "Australia", exchangeRate: 7000 },
+  { number: "4", currencyName: "Amar", country: "India", exchangeRate: 18000 },
+  { number: "5", currencyName: "Lakshmi", country: "India", exchangeRate: 12000 },
+  { number: "6", currencyName: "James", country: "Canada", exchangeRate: 50000 },
+  { number: "7", currencyName: "Ronald", country: "US", exchangeRate: 75000 },
+  { number: "8", currencyName: "Mike", country: "Belgium", exchangeRate: 100000 },
+  { number: "9", currencyName: "Andrew", country: "Argentina", exchangeRate: 45000 },
+  { number: "10", currencyName: "Stephen", country: "Austria", exchangeRate: 30000 },
+  { number: "11", currencyName: "Sara", country: "China", exchangeRate: 750000 },
+  { number: "12", currencyName: "JonRoot", country: "Argentina", exchangeRate: 65000 },
+];
+
+export const CurrencyExchange = () => {
+  return (
+    <div className="container mt-4">
+      <div className="text-center text-success mb-3">
+        <h3>Bootstrap</h3>
+      </div>
+      <DataTable columns={columns} data={data} pagination striped />
+    </div>
+  );
+};
+
+export default CurrencyExchange;
